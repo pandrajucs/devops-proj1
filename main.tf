3ad1aa4b63b6e61bc202698a4028c741b330db57
C S <pandrajucs@example.com>
2020-09-30 17:23:27 +0530

    third commit

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
+   
+    region = "us-east-1"
     
 }
 
@@ -26,46 +25,46 @@ resource "aws_security_group" "allow_all" {
 }
 
 resource "aws_instance" "server-1" {
-    ami = "ami-00514a528eadbc95b"
+    ami = "ami-086f5be371345273e"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
     key_name = "AWS_KeyPair"
     subnet_id = "subnet-034250c45e4a08fbf"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
-    user_data = "${file("script1.sh")}"
+    
     tags = {
-        Name = "Server-1"
+        Name = "Ansible-Node-1"
     }
 } 
 
 
 
 resource "aws_instance" "server-2" {
-   ami = "ami-00514a528eadbc95b"
+   ami = "ami-086f5be371345273e"
    availability_zone = "us-east-1b"
    instance_type = "t2.micro"
    key_name = "AWS_KeyPair"
    subnet_id = "subnet-0d47b4ff30b766a68"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
-    user_data = "${file("script2.sh")}"
+   
     tags = {
-       Name = "Server-2"
+       Name = "Ansible-Node-2"
    }
 } 
 
 resource "aws_instance" "server-3" {
-   ami = "ami-00514a528eadbc95b"
+   ami = "ami-086f5be371345273e"
    availability_zone = "us-east-1c"
    instance_type = "t2.micro"
    key_name = "AWS_KeyPair"
    subnet_id = "subnet-0e603ab3ebf20d410"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
-    user_data = "${file("script3.sh")}"
+    
     tags = {
-       Name = "Server-3"
+       Name = "Ansible-Node-3"
    }
 } 
 
@@ -84,7 +83,5 @@ resource "aws_lb" "nlb" {
 
 } 
 
-data "aws_route53_zone" "r53" {
-  name = "pandrajucs.in"
-}
+
 
