87aa63fbf2ba1dd1a4b6b2a33adf7dde52d1c5b1
C S <pandrajucs@example.com>
2020-09-30 17:17:11 +0530

    second commit

--- targetgroup.tf
+++ targetgroup.tf
@@ -2,21 +2,21 @@ resource "aws_lb_target_group_attachment" "tg1" {
   target_group_arn = "${aws_lb_target_group.tg.arn}"
   target_id        = "${aws_instance.server-1.id}"
   port             = 80
-  depends_on = "${aws_instance.server-1}"
+
 }
 
 resource "aws_lb_target_group_attachment" "tg2" {
   target_group_arn = "${aws_lb_target_group.tg.arn}"
   target_id        = "${aws_instance.server-2.id}"
   port             = 80
-  depends_on = "${aws_instance.server-2}"
+  
 }
 
 resource "aws_lb_target_group_attachment" "tg3" {
   target_group_arn = "${aws_lb_target_group.tg.arn}"
   target_id        = "${aws_instance.server-3.id}"
   port             = 80
-  depends_on = "${aws_instance.server-3}"
+  
 }
 
 
@@ -28,5 +28,5 @@ resource "aws_lb_listener" "web" {
     type             = "forward"
     target_group_arn = "${aws_lb_target_group.tg.arn}"
   }
-  depends_on = "${aws_lb.nlb}"
+ 
 }
